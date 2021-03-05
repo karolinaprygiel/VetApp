2c0924570bc486befc6d83344d7a1512597a2bbe
Mateusz <7277821-mateuszgwozdz@users.noreply.gitlab.com>
2021-03-05 15:06:42 +0100

    print hello

--- src/main/java/uj/jwzp2021/gp/VetApp/VetAppApplication.java
+++ src/main/java/uj/jwzp2021/gp/VetApp/VetAppApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VetAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(VetAppApplication.class, args);
+
+		System.out.println("Hello!");
 	}
 
 }
