abcd45ac456cd3b799a01e2bab151d6c3a55c37d
Mateusz <7277821-mateuszgwozdz@users.noreply.gitlab.com>
2021-04-15 22:49:29 +0200

    Update Clinic [skip ci]

--- src/main/java/uj/jwzp2021/gp/VetApp/model/entity/Clinic.java
+++ src/main/java/uj/jwzp2021/gp/VetApp/model/entity/Clinic.java
@@ -1,10 +1,19 @@
 package uj.jwzp2021.gp.VetApp.model.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "clinic")
 public class Clinic {
 
     @Id
