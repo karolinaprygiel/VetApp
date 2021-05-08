690e394969145d7a1b325a1d010ee82430bdc6bf
karolinaprygiel <pkarolina890@gmail.com>
2021-05-09 00:29:02 +0200

    Fix flyway

---
+++ src/main/resources/db/migration/V1__create_Schema.sql
@@ -0,0 +1,44 @@
+create table clients
+(
+    id SERIAL PRIMARY KEY,
+    name varchar(255),
+    surname varchar(255)
+);
+
+
+
+create table animals(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(127),
+    type INTEGER ,
+    year_of_birth INTEGER NOT NULL,
+    month_o
+    client_id INTEGER REFERENCES clients(id)
+    );
+
+create table  vets
+(
+    id SERIAL PRIMARY KEY,
+    name varchar(255),
+    shift_end timestamp,
+    shift_start timestamp,
+    surname varchar(255)
+    );
+
+create table visits
+(
+    id SERIAL PRIMARY KEY,
+    description varchar(255),
+    duration interval,
+    price numeric(19,2),
+    start_time timestamp,
+    status integer,
+    animal_id INTEGER references animals(id),
+    client_id INTEGER references clients(id),
+    vet_id INTEGER references vets(id)
+    );
+
+
+
+
+
