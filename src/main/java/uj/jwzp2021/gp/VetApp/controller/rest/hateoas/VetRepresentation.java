f52c7a306b258ee980e691b7c4e1fc9bcfe9a879
Mateusz <matgw123@gmail.com>
2021-05-12 18:19:05 +0200

    Add more Hateoas [skip ci]

--- src/main/java/uj/jwzp2021/gp/VetApp/controller/rest/hateoas/VetRepresentation.java
+++ src/main/java/uj/jwzp2021/gp/VetApp/controller/rest/hateoas/VetRepresentation.java
@@ -3,23 +3,23 @@ package uj.jwzp2021.gp.VetApp.controller.rest.hateoas;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.hateoas.RepresentationModel;
-import uj.jwzp2021.gp.VetApp.model.dto.Responses.ClientResponseDto;
+import uj.jwzp2021.gp.VetApp.model.dto.Responses.VetResponseDto;
 
 @Data
 @AllArgsConstructor
-public class VetRepresentation extends RepresentationModel<ClientRepresentation> {
+public class VetRepresentation extends RepresentationModel<VetRepresentation> {
   private final int id;
   private final String name;
   private final String surname;
   private final String shiftStart;
   private final String shiftEnd;
 
-  public static ClientRepresentation fromClientResponseDto(ClientResponseDto clientResponseDto) {
-    return new ClientRepresentation(
-        clientResponseDto.getId(),
-        clientResponseDto.getName(),
-        clientResponseDto.getSurname()
-//        clientResponseDto.
-    );
+  public static VetRepresentation fromVetResponseDto(VetResponseDto vetResponseDto) {
+    return new VetRepresentation(
+        vetResponseDto.getId(),
+        vetResponseDto.getName(),
+        vetResponseDto.getSurname(),
+        vetResponseDto.getShiftStart(),
+        vetResponseDto.getShiftEnd());
   }
 }
