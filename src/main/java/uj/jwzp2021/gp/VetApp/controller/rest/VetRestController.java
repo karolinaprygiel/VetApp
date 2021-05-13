eb0435274afd95a3c796b2b4378da7ea805b5edf
Mateusz <matgw123@gmail.com>
2021-05-13 20:37:32 +0200

    Update Vet's HATEOAS [skip ci]

--- src/main/java/uj/jwzp2021/gp/VetApp/controller/rest/VetRestController.java
+++ src/main/java/uj/jwzp2021/gp/VetApp/controller/rest/VetRestController.java
@@ -8,6 +8,7 @@ import uj.jwzp2021.gp.VetApp.controller.rest.hateoas.VetRepresentation;
 import uj.jwzp2021.gp.VetApp.model.dto.Requests.VetRequestDto;
 import uj.jwzp2021.gp.VetApp.model.dto.Responses.VetResponseDto;
 import uj.jwzp2021.gp.VetApp.service.VetService;
+import uj.jwzp2021.gp.VetApp.service.VisitService;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,15 +21,25 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 public class VetRestController {
 
   private final VetService vetService;
+  private final VisitService visitService;
 
   @Autowired
-  public VetRestController(VetService vetService) {
+  public VetRestController(VetService vetService, VisitService visitService) {
     this.vetService = vetService;
+    this.visitService = visitService;
   }
 
   @GetMapping(path = "{id}")
   public ResponseEntity<?> getVet(@PathVariable int id) {
     return ResponseEntity.ok(vetService.getVetById(id));
+  }
+
+  @GetMapping(path = "{id}/visits")
+  public ResponseEntity<?> getVetVisits(@PathVariable int id) {
+    return ResponseEntity.ok(
+        vetService.getVetById(id).getVisitIds().stream()
+            .map(visitService::getVisitById)
+            .collect(Collectors.toList()));
   }
 
   @GetMapping(path = "{id}/hateoas")
@@ -62,13 +73,7 @@ public class VetRestController {
   private VetRepresentation represent(VetResponseDto v) {
     var representation = VetRepresentation.fromVetResponseDto(v);
     representation.add(linkTo(methodOn(VetRestController.class).getVet(v.getId())).withSelfRel());
-    representation.add(
-        v.getVisitIds().stream()
-            .map(
-                (id) ->
-                    linkTo(methodOn(VisitsRestController.class).getVisit(id))
-                        .withRel("oneOfVisits"))
-            .collect(Collectors.toList()));
+    representation.add(linkTo(methodOn(VetRestController.class).getVetVisits(v.getId())).withRel("visits"));
     return representation;
   }
 }
