d0c0ff3a3d23fa49e2340857f88806117aa3579f
Mateusz <matgw123@gmail.com>
2021-05-16 19:55:07 +0200

    Add logs [skip ci]

--- src/main/java/uj/jwzp2021/gp/VetApp/service/AnimalService.java
+++ src/main/java/uj/jwzp2021/gp/VetApp/service/AnimalService.java
@@ -26,24 +26,29 @@ public class AnimalService {
   }
 
   public Animal getAnimalById(int id) {
+    log.debug("Looking up animal with id=" + id);
     var animal = animalRepository.findById(id);
     return animal.orElseThrow(
         () -> {
+          log.debug("Not found animal with id=" + id);
           throw new AnimalNotFoundException("Animal with id:" + id + " not found.");
         });
   }
 
   public List<Animal> getAllAnimals() {
+    log.debug("Returning all animals");
     return new ArrayList<>(animalRepository.findAll());
   }
 
   public Animal deleteAnimal(int id) {
+    log.debug("Deleting animal with id=" + id);
     var animal = getAnimalById(id);
     animalRepository.delete(animal);
     return animal;
   }
 
   public Animal createAnimal(AnimalRequestDto animalRequestDto) {
+    log.debug("Creating animal for: " + animalRequestDto);
     var owner = clientService.getClientById(animalRequestDto.getOwnerId());
     return animalRepository.save(AnimalMapper.toAnimal(animalRequestDto, owner));
   }
