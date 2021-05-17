be9c144283ea610ddb737a35eda691ec76ec40d2
karolinaprygiel <pkarolina890@gmail.com>
2021-05-17 16:35:05 +0200

    Refactor isVetAtWork

--- src/main/java/uj/jwzp2021/gp/VetApp/service/VetService.java
+++ src/main/java/uj/jwzp2021/gp/VetApp/service/VetService.java
@@ -10,9 +10,9 @@ import uj.jwzp2021.gp.VetApp.repository.VetRepository;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class VetService {
@@ -45,9 +45,57 @@ public class VetService {
     return vet;
   }
 
-  public boolean vetAvailable(LocalDateTime startTime, Duration duration, int vetId) {
+  public boolean worksAtMidnight(Vet vet) {
+    var startTime = vet.getShiftStart();
+    var endTime = vet.getShiftStart().plus(vet.getWorkingTime());
+    return startTime.isAfter(endTime);
+  }
+
+  public boolean isVetAtWork(LocalDateTime startTime, Duration duration, int vetId) {
     var vet = getVetById(vetId);
-    return startTime.plus(duration).toLocalTime().isBefore(vet.getShiftEnd().plusSeconds(1))
-        && startTime.toLocalTime().isAfter(vet.getShiftStart().minusSeconds(1));
+
+    // vet 15.04 20:00 - 16.04 4:00
+    // wizyty:
+    // 15.04 21:00 - 15 MIN
+    // 15.04 23:55 - 15 MIN koniec 16.04
+    // 16.04 02:22 - 15 MIN
+    // Vet - start datetime i duration
+    // Wizyta - start time i duration
+
+    var worksAtMidnight = worksAtMidnight(vet);
+
+    if (!worksAtMidnight) {
+      /* simplest case - vet works regular shift*/
+      var vetWorkStart = vet.getShiftStart().atDate(startTime.toLocalDate());
+      var vetWorkEnd =
+          vet.getShiftStart().plus(vet.getWorkingTime()).atDate(startTime.toLocalDate());
+      return startTime.isAfter(vetWorkStart.minusSeconds(1))
+          && startTime.plus(duration).isBefore(vetWorkEnd.plusSeconds(1));
+    } else {
+      /* two cases to consider when vet is at work at midnight */
+      var visitCrossesMidnight =
+          startTime.toLocalTime().isAfter(startTime.toLocalTime().plus(duration));
+      if (visitCrossesMidnight) {
+        /* vet and visit "synchronize" - relatively simple case */
+        var vetWorkStart = vet.getShiftStart().atDate(startTime.toLocalDate());
+        var vetWorkEnd =
+            vet.getShiftStart()
+                .plus(vet.getWorkingTime())
+                .atDate(startTime.toLocalDate().plusDays(1));
+        return startTime.isAfter(vetWorkStart.minusSeconds(1))
+            && startTime.plus(duration).isBefore(vetWorkEnd.plusSeconds(1));
+      } else {
+        /* vet works at midnight, but visit does not cross it - let's split the vet's shift into two shifts */
+        var vetWorkStart1= LocalDateTime.of(startTime.toLocalDate(), LocalTime.MIDNIGHT);;
+        var vetWorkEnd1 = LocalDateTime.of(startTime.toLocalDate().minusDays(1), vet.getShiftStart()).plus(vet.getWorkingTime());
+
+        var vetWorkStart2 = LocalDateTime.of(startTime.toLocalDate(), vet.getShiftStart());
+        var vetWorkEnd2 = LocalDateTime.of(startTime.toLocalDate(), LocalTime.of(23,59,59, 59));
+        return (startTime.isAfter(vetWorkStart1.minusSeconds(1))
+                && startTime.plus(duration).isBefore(vetWorkEnd1.plusSeconds(1)))
+            || (startTime.isAfter(vetWorkStart2.minusSeconds(1))
+                && startTime.plus(duration).isBefore(vetWorkEnd2.plusSeconds(1)));
+      }
+    }
   }
 }
