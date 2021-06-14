9ab36719bba5fcfeca99aa7b24d438b3cbe600d2
Mateusz <matgw123@gmail.com>
2021-06-14 15:30:23 +0200

    Add role to response

--- src/main/java/uj/jwzp2021/gp/VetApp/controller/rest/hateoas/UserRepresentation.java
+++ src/main/java/uj/jwzp2021/gp/VetApp/controller/rest/hateoas/UserRepresentation.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.hateoas.RepresentationModel;
 import uj.jwzp2021.gp.VetApp.model.entity.User;
+import uj.jwzp2021.gp.VetApp.security.Role;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -12,8 +13,9 @@ import uj.jwzp2021.gp.VetApp.model.entity.User;
 public class UserRepresentation extends RepresentationModel<UserRepresentation> {
     private final int id;
     private final String username;
+    private final Role role;
 
     public static UserRepresentation fromUser(User user) {
-        return new UserRepresentation(user.getId(), user.getUsername());
+        return new UserRepresentation(user.getId(), user.getUsername(), user.getRole());
     }
 }
