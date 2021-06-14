32baa6d22827047f1179ac90b04df2c5129fc6dd
Mateusz <matgw123@gmail.com>
2021-06-14 13:53:58 +0200

    QuickFix

--- src/main/java/uj/jwzp2021/gp/VetApp/security/MyUserDetailsService.java
+++ src/main/java/uj/jwzp2021/gp/VetApp/security/MyUserDetailsService.java
@@ -4,15 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import uj.jwzp2021.gp.VetApp.service.UserService;
 
 
-@Service
+@Component
 public class MyUserDetailsService implements UserDetailsService {
 
+    final UserService userService;
+
     @Autowired
-    UserService userService;
+    public MyUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
